7fab6afbe66b461812278885864deec5ea9037bc
Shivansh Shukla <shivanshshukla60@gmail.com>
2021-12-06 23:47:24 +0530

    added comment

--- mutilply.cpp
+++ mutilply.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    // Ignore spelling mistake in file name :)
+    // Ignore spelling mistake in file name lol
     int a,b;
     cin>>a>>b;
     cout<<a*b<<endl;
